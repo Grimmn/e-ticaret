6e078f46044bb847e7297a7f7e6f072bb323c179
Utku Enes Alagöz <utkuenesalagoz@icloud.com>
2022-09-26 13:11:11 +0300

    Initial Commit

---
+++ e-ticaret/e_ticaretApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_ticaretApp.swift
+//  e-ticaret
+//
+//  Created by utku enes alagöz on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct e_ticaretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
